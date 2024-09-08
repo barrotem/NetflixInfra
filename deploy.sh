f0bb9d4147969c40f2cfe2d41b21b24f1264a4a1
BarRotem <bar.rotem45@gmail.com>
2024-09-08 23:59:08 +0300

    fix deploy - abs paths global

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@
 #3. The script is statically typed, addressing Nginx configuration files with their default names (e.g. nginx.conf).
 
 #Remove erroneous, old key files
-if [[ -f "temp_key.pem" ]]
+if [[ -f "/home/ubuntu/NginxInfra/temp_key.pem" ]]
 then
   echo "temp_key.pem exists. removing"
-  rm temp_key.pem
+  rm /home/ubuntu/NginxInfra/temp_key.pem
 fi
 
 #Replace old nginx.conf with the new one.
